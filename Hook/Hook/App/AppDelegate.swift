65bd85418a8a5558d8e6eb1d510267208b4eb57e
meregrey <yeojinyoon.dev@gmail.com>
2021-12-21 11:48:18 +0900

    프로젝트 생성

---
+++ Hook/Hook/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Hook
+//
+//  Created by Yeojin Yoon on 2021/12/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
