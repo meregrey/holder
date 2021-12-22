27f6fbef1cccd249ca4e4b3377439557b545ad6b
meregrey <yeojinyoon.dev@gmail.com>
2021-12-22 23:30:26 +0900

    Result 타입을 반환하여 케이스에 따라 직관적으로 처리할 수 있도록 개선

--- Hook/Hook/Repositories/CredentialRepository.swift
+++ Hook/Hook/Repositories/CredentialRepository.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 protocol CredentialRepositoryType {
-    func save(_ credential: Credential) throws
-    func fetch() throws -> Credential?
-    func delete() throws
+    func save(_ credential: Credential) -> Result<Void, Error>
+    func fetch() -> Result<Credential?, Error>
+    func delete() -> Result<Void, Error>
 }
 
 final class CredentialRepository: CredentialRepositoryType {
@@ -24,28 +24,43 @@ final class CredentialRepository: CredentialRepositoryType {
         self.keychainManager = keychainManager
     }
     
-    func save(_ credential: Credential) throws {
-        let attributes = [kSecAttrService: bundleIdentifier, kSecAttrAccount: credential.name]
-        try keychainManager.addItem(credential.identifier,
-                                    itemClass: kSecClassGenericPassword,
-                                    itemAttributes: attributes)
+    func save(_ credential: Credential) -> Result<Void, Error> {
+        do {
+            let attributes = [kSecAttrService: bundleIdentifier, kSecAttrAccount: credential.name]
+            try keychainManager.addItem(credential.identifier,
+                                        itemClass: kSecClassGenericPassword,
+                                        itemAttributes: attributes)
+            return .success(())
+        } catch {
+            return .failure(error)
+        }
     }
     
-    func fetch() throws -> Credential? {
-        guard currentCredential == nil else { return currentCredential }
-        guard let item: KeychainItem<String> = try keychainManager.searchItem(ofClass: kSecClassGenericPassword,
-                                                                              itemAttributes: [kSecAttrService: bundleIdentifier]) else { return nil }
-        guard let name = item.account else { return nil }
+    func fetch() -> Result<Credential?, Error> {
+        guard currentCredential == nil else { return .success(currentCredential) }
+        var item: KeychainItem<String>? = nil
+        do {
+            item = try keychainManager.searchItem(ofClass: kSecClassGenericPassword, itemAttributes: [kSecAttrService: bundleIdentifier])
+        } catch {
+            return .failure(error)
+        }
+        guard let item = item else { return .success(nil) }
+        guard let name = item.account else { return .success(nil) }
         currentCredential = Credential(identifier: item.value, name: name)
-        return currentCredential
+        return .success(currentCredential)
     }
     
-    func delete() throws {
-        guard let credential = currentCredential else { return }
-        let attributes = [kSecAttrService: bundleIdentifier, kSecAttrAccount: credential.name]
-        try keychainManager.deleteItem(credential.identifier,
-                                       itemClass: kSecClassGenericPassword,
-                                       itemAttributes: attributes)
-        currentCredential = nil
+    func delete() -> Result<Void, Error> {
+        guard let credential = currentCredential else { return .success(()) }
+        do {
+            let attributes = [kSecAttrService: bundleIdentifier, kSecAttrAccount: credential.name]
+            try keychainManager.deleteItem(credential.identifier,
+                                           itemClass: kSecClassGenericPassword,
+                                           itemAttributes: attributes)
+            currentCredential = nil
+            return .success(())
+        } catch {
+            return .failure(error)
+        }
     }
 }
