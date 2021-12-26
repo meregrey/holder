f675a4dfbf1229ff4086f3d8a92b48d6f86a340d
meregrey <yeojinyoon.dev@gmail.com>
2021-12-26 23:57:28 +0900

    Data 타입과의 변환을 충족하도록 요구하는 Convertible 프로토콜 정의

---
+++ Hook/Hook/Utils/Convertible.swift
@@ -0,0 +1,23 @@
+//
+//  Convertible.swift
+//  Hook
+//
+//  Created by Yeojin Yoon on 2021/12/26.
+//
+
+import Foundation
+
+protocol Convertible {
+    static func converted(from data: Data) -> Self?
+    func converted() -> Data
+}
+
+extension String: Convertible {
+    static func converted(from data: Data) -> String? {
+        return String(data: data, encoding: .utf8)
+    }
+    
+    func converted() -> Data {
+        return Data(self.utf8)
+    }
+}
