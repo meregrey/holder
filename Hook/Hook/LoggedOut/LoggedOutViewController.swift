4de7904575be9c8a111cc7d435aec0ff50f00f44
meregrey <yeojinyoon.dev@gmail.com>
2021-12-27 00:03:00 +0900

    Sign in with Apple UI 구성 및 동작 구현

--- Hook/Hook/LoggedOut/LoggedOutViewController.swift
+++ Hook/Hook/LoggedOut/LoggedOutViewController.swift
@@ -5,13 +5,75 @@
 //  Created by Yeojin Yoon on 2021/12/24.
 //
 
+import AuthenticationServices
 import RIBs
 import RxSwift
 import UIKit
 
-protocol LoggedOutPresentableListener: AnyObject {}
+protocol LoggedOutPresentableListener: AnyObject {
+    func didSucceedLogin(withCredential credential: Credential)
+}
 
 final class LoggedOutViewController: UIViewController, LoggedOutPresentable, LoggedOutViewControllable {
+    
+    @AutoLayout private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        return stackView
+    }()
 
     weak var listener: LoggedOutPresentableListener?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViews()
+    }
+    
+    private func configureViews() {
+        view.backgroundColor = .white
+        view.addSubview(stackView)
+        configureLoginButton()
+        NSLayoutConstraint.activate([
+            stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: Size.widthMultiplier),
+            stackView.heightAnchor.constraint(equalToConstant: Size.heightForLoginButton),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: Size.bottomMarginForLoginButton)
+        ])
+    }
+    
+    private func configureLoginButton() {
+        let loginButton = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
+        loginButton.cornerRadius = Size.heightForLoginButton
+        loginButton.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
+        stackView.addArrangedSubview(loginButton)
+    }
+    
+    @objc private func loginButtonDidTap() {
+        let provider = ASAuthorizationAppleIDProvider()
+        let request = provider.createRequest()
+        request.requestedScopes = [.fullName]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+    }
+}
+
+extension LoggedOutViewController: ASAuthorizationControllerDelegate {
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        guard let authorizationCredential = authorization.credential as? ASAuthorizationAppleIDCredential else { return }
+        guard let name = authorizationCredential.fullName.map({ PersonNameComponentsFormatter.localizedString(from: $0, style: .default) }) else { return }
+        let credential = Credential(identifier: authorizationCredential.user, name: name)
+        listener?.didSucceedLogin(withCredential: credential)
+    }
+}
+
+extension LoggedOutViewController: ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window ?? ASPresentationAnchor()
+    }
 }
