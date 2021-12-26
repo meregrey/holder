0e2760de950ffebe2d45a9366b0b8395c5b9a4e0
meregrey <yeojinyoon.dev@gmail.com>
2021-12-27 00:02:31 +0900

    UI 사이즈 관련 상수를 관리하기 위한 Size 열거형 정의

---
+++ Hook/Hook/Utils/Size.swift
@@ -0,0 +1,14 @@
+//
+//  Size.swift
+//  Hook
+//
+//  Created by Yeojin Yoon on 2021/12/25.
+//
+
+import CoreGraphics
+
+enum Size {
+    static let widthMultiplier: CGFloat = 0.8
+    static let heightForLoginButton: CGFloat = 50
+    static let bottomMarginForLoginButton: CGFloat = -180
+}
