6fc093536eede62c0f9c131b9b555c587645e477
meregrey <yeojinyoon.dev@gmail.com>
2021-12-30 23:48:42 +0900

    로컬라이제이션 관련 연산 프로퍼티를 제공하기 위해 String 타입 확장

---
+++ Hook/Hook/Utils/Extensions/String.swift
@@ -0,0 +1,12 @@
+//
+//  String.swift
+//  Hook
+//
+//  Created by Yeojin Yoon on 2021/12/29.
+//
+
+import Foundation
+
+extension String {
+    var localized: Self { NSLocalizedString(self, comment: "") }
+}
