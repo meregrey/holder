9a252b3e38f0b38bb3fe51be7181f9bd40f4025c
meregrey <yeojinyoon.dev@gmail.com>
2021-12-30 23:47:11 +0900

    노티피케이션 이름 관련 상수를 관리하기 위한 NotificationName 열거형 정의

---
+++ Hook/Hook/Utils/Constants/NotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName.swift
+//  Hook
+//
+//  Created by Yeojin Yoon on 2021/12/29.
+//
+
+import Foundation
+
+enum NotificationName {
+    static let didFailToSaveCredential = NSNotification.Name("didFailToSaveCredential")
+}
