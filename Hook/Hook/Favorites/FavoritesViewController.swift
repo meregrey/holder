cac3b873cace8f98868d0bc5dd71f940d7505df3
meregrey <yeojinyoon.dev@gmail.com>
2022-01-03 17:59:35 +0900

    Favorites 뷰의 title 및 tabBarItem 설정

--- Hook/Hook/Favorites/FavoritesViewController.swift
+++ Hook/Hook/Favorites/FavoritesViewController.swift
@@ -13,4 +13,19 @@ protocol FavoritesPresentableListener: AnyObject {}
 final class FavoritesViewController: UIViewController, FavoritesPresentable, FavoritesViewControllable {
 
     weak var listener: FavoritesPresentableListener?
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        configureViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureViews()
+    }
+    
+    private func configureViews() {
+        title = LocalizedString.ViewTitle.favorites
+        tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "bookmark"), selectedImage: UIImage(systemName: "bookmark.fill"))
+    }
 }
