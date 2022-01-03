3a91ab1d24aa37990208a4cb4b36690e5a2ae0b9
meregrey <yeojinyoon.dev@gmail.com>
2022-01-03 17:59:57 +0900

    Account 뷰의 title 및 tabBarItem 설정

--- Hook/Hook/Account/AccountViewController.swift
+++ Hook/Hook/Account/AccountViewController.swift
@@ -13,4 +13,19 @@ protocol AccountPresentableListener: AnyObject {}
 final class AccountViewController: UIViewController, AccountPresentable, AccountViewControllable {
 
     weak var listener: AccountPresentableListener?
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        configureViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureViews()
+    }
+    
+    private func configureViews() {
+        title = LocalizedString.ViewTitle.account
+        tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person.crop.circle"), selectedImage: UIImage(systemName: "person.crop.circle.fill"))
+    }
 }
