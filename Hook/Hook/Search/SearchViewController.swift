1cb34845b1ffee062ade5ee18237be5ba7722812
meregrey <yeojinyoon.dev@gmail.com>
2022-01-03 17:59:09 +0900

    Search 뷰의 title 및 tabBarItem 설정

--- Hook/Hook/Search/SearchViewController.swift
+++ Hook/Hook/Search/SearchViewController.swift
@@ -13,4 +13,19 @@ protocol SearchPresentableListener: AnyObject {}
 final class SearchViewController: UIViewController, SearchPresentable, SearchViewControllable {
 
     weak var listener: SearchPresentableListener?
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        configureViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureViews()
+    }
+    
+    private func configureViews() {
+        title = LocalizedString.ViewTitle.search
+        tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "magnifyingglass"), selectedImage: UIImage(systemName: "magnifyingglass"))
+    }
 }
