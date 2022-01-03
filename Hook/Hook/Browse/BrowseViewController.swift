05bcd4874b77159932ad0a454c2072b931caf9a7
meregrey <yeojinyoon.dev@gmail.com>
2022-01-03 17:58:52 +0900

    Browse 뷰의 title 및 tabBarItem 설정

--- Hook/Hook/Browse/BrowseViewController.swift
+++ Hook/Hook/Browse/BrowseViewController.swift
@@ -13,4 +13,19 @@ protocol BrowsePresentableListener: AnyObject {}
 final class BrowseViewController: UIViewController, BrowsePresentable, BrowseViewControllable {
 
     weak var listener: BrowsePresentableListener?
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        configureViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureViews()
+    }
+    
+    private func configureViews() {
+        title = LocalizedString.ViewTitle.browse
+        tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "rectangle.grid.1x2"), selectedImage: UIImage(systemName: "rectangle.grid.1x2.fill"))
+    }
 }
