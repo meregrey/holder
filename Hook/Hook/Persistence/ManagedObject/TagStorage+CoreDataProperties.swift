f271bb96935a958095e8bc42d82bcc3d14e0ca38
meregrey <yeojinyoon.dev@gmail.com>
2022-01-06 18:53:25 +0900

    NSManagedObject를 상속하는 TagStorage 클래스 정의

---
+++ Hook/Hook/Persistence/ManagedObject/TagStorage+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  TagStorage+CoreDataProperties.swift
+//  Hook
+//
+//  Created by Yeojin Yoon on 2022/01/04.
+//
+
+import CoreData
+import Foundation
+
+extension TagStorage {
+    
+    @NSManaged public var tags: [TagEntity]
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<TagStorage> {
+        return NSFetchRequest<TagStorage>(entityName: "TagStorage")
+    }
+}
+
+extension TagStorage: Identifiable {}
