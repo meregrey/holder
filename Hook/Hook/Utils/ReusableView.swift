949a518ffa35cd8e8fd042c0e97c96486302fd6e
meregrey <yeojinyoon.dev@gmail.com>
2022-01-09 23:09:34 +0900

    UIView 타입에 reuseIdentifier를 제공하기 위한 ReusableView 프로토콜 정의

---
+++ Hook/Hook/Utils/ReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  ReusableView.swift
+//  Hook
+//
+//  Created by Yeojin Yoon on 2022/01/09.
+//
+
+import UIKit
+
+protocol ReusableView: AnyObject {}
+
+extension ReusableView where Self: UIView {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UICollectionViewCell: ReusableView {}
