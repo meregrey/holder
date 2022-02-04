2721da54f435afc84963690edf49655d71bd086c
meregrey <yeojinyoon.dev@gmail.com>
2022-02-04 16:44:19 +0900

    Asset 열거형에 컬러 관련 상수 추가

--- Hook/Hook/Utils/Constants/Asset.swift
+++ Hook/Hook/Utils/Constants/Asset.swift
@@ -12,10 +12,16 @@ enum Asset {
         static let primaryColor = UIColor(named: "PrimaryColor") ?? .clear
         static let secondaryColor = UIColor(named: "SecondaryColor") ?? .clear
         static let tertiaryColor = UIColor(named: "TertiaryColor") ?? .clear
+        
         static let baseBackgroundColor = UIColor(named: "BaseBackgroundColor") ?? .clear
         static let upperBackgroundColor = UIColor(named: "UpperBackgroundColor") ?? .clear
         static let selectedBackgroundColor = UIColor(named: "SelectedBackgroundColor") ?? .clear
-        static let sheetBackgroundColor = UIColor(named: "SheetBackgroundColor") ?? .clear
+        static let sheetBaseBackgroundColor = UIColor(named: "SheetBaseBackgroundColor") ?? .clear
+        static let sheetUpperBackgroundColor = UIColor(named: "SheetUpperBackgroundColor") ?? .clear
+        
         static let borderColor = UIColor(named: "BorderColor") ?? .clear
+        static let sheetBorderColor = UIColor(named: "SheetBorderColor") ?? .clear
+        
+        static let sheetLabelColor = UIColor(named: "SheetLabelColor") ?? .clear
     }
 }
