dfa86ce8140524f515dd92628dbc1b4162e7ee45
meregrey <yeojinyoon.dev@gmail.com>
2022-02-15 10:47:19 +0900

    TextField의 이니셜라이저 파라미터 및 프로퍼티 변경

--- Hook/Hook/Views/TextField.swift
+++ Hook/Hook/Views/TextField.swift
@@ -10,27 +10,31 @@ import UIKit
 final class TextField: UITextField {
     
     private enum Metric {
-        static let inset = CGFloat(12)
+        static let inset = CGFloat(16)
         static let offset = CGFloat(-6)
-        static let height = CGFloat(52)
-        static let borderWidth = CGFloat(1)
+        static let height = CGFloat(55)
         static let cornerRadius = CGFloat(12)
+        static let borderWidth = CGFloat(1)
     }
     
     private enum Font {
         static let text = UIFont.systemFont(ofSize: 17, weight: .medium)
     }
     
-    init(placeholder: String? = nil) {
+    init(placeholder: String? = nil,
+         keyboardType: UIKeyboardType = .default,
+         theme: ViewTheme = .normal) {
         super.init(frame: .zero)
         self.placeholder = placeholder
-        configure()
+        self.keyboardType = keyboardType
+        configure(with: theme)
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         self.placeholder = nil
-        configure()
+        self.keyboardType = .default
+        configure(with: .normal)
     }
     
     override func textRect(forBounds bounds: CGRect) -> CGRect {
@@ -48,15 +52,23 @@ final class TextField: UITextField {
         return originalRect.offsetBy(dx: Metric.offset, dy: 0)
     }
     
-    private func configure() {
+    private func configure(with theme: ViewTheme) {
         translatesAutoresizingMaskIntoConstraints = false
         heightAnchor.constraint(equalToConstant: Metric.height).isActive = true
         font = Font.text
         clearButtonMode = .whileEditing
+        layer.cornerRadius = Metric.cornerRadius
+        layer.cornerCurve = .continuous
+        
+        if theme == .sheet {
+            textColor = Asset.Color.sheetLabelColor
+            backgroundColor = Asset.Color.sheetUpperBackgroundColor
+            return
+        }
+        
+        textColor = Asset.Color.primaryColor
         backgroundColor = Asset.Color.upperBackgroundColor
         layer.borderColor = Asset.Color.borderColor.cgColor
         layer.borderWidth = Metric.borderWidth
-        layer.cornerRadius = Metric.cornerRadius
-        layer.cornerCurve = .continuous
     }
 }
