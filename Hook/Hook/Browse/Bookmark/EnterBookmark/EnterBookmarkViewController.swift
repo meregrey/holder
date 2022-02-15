725b2d2db2e62a4af8f4d56cb66af78b9b55019a
meregrey <yeojinyoon.dev@gmail.com>
2022-02-15 10:47:58 +0900

    EnterBookmarkViewController에 URL을 입력하기 위한 텍스트 필드 추가

--- Hook/Hook/Browse/Bookmark/EnterBookmark/EnterBookmarkViewController.swift
+++ Hook/Hook/Browse/Bookmark/EnterBookmark/EnterBookmarkViewController.swift
@@ -18,12 +18,21 @@ final class EnterBookmarkViewController: UIViewController, EnterBookmarkPresenta
     
     @AutoLayout private var headerView = SheetHeaderView(title: LocalizedString.ViewTitle.addBookmark)
     
-    @AutoLayout private var tagCollectionView = LabeledTagCollectionView(title: LocalizedString.LabelTitle.tags)
+    @AutoLayout private var scrollView = UIScrollView()
+    
+    @AutoLayout private var urlTextField = LabeledTextField(header: "URL",
+                                                            keyboardType: .URL,
+                                                            theme: .sheet)
+    
+    @AutoLayout private var tagCollectionView = LabeledTagCollectionView(header: LocalizedString.LabelTitle.tags)
     
     private enum Metric {
-        static let tagCollectionViewCellHeight = CGFloat(36)
+        static let urlTextFieldTop = CGFloat(20)
+        static let urlTextFieldLeading = CGFloat(20)
+        static let urlTextFieldTrailing = CGFloat(-20)
         
-        static let tagCollectionViewTop = CGFloat(20)
+        static let tagCollectionViewCellHeight = CGFloat(36)
+        static let tagCollectionViewTop = CGFloat(40)
         static let tagCollectionViewLeading = CGFloat(20)
         static let tagCollectionViewTrailing = CGFloat(-20)
     }
@@ -40,6 +49,11 @@ final class EnterBookmarkViewController: UIViewController, EnterBookmarkPresenta
         configureViews()
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        urlTextField.becomeFirstResponder()
+    }
+    
     func update(with tags: [Tag]) {
         self.selectedTags = tags
         tagCollectionView.reloadData()
@@ -47,6 +61,8 @@ final class EnterBookmarkViewController: UIViewController, EnterBookmarkPresenta
     }
     
     private func configureViews() {
+        urlTextField.delegate = self
+        
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tagCollectionViewDidTap(_:)))
         tagCollectionView.addGestureRecognizer(tapGestureRecognizer)
         tagCollectionView.dataSource = self
@@ -54,16 +70,32 @@ final class EnterBookmarkViewController: UIViewController, EnterBookmarkPresenta
         
         view.backgroundColor = Asset.Color.sheetBaseBackgroundColor
         view.addSubview(headerView)
-        view.addSubview(tagCollectionView)
+        view.addSubview(scrollView)
+        
+        scrollView.addSubview(urlTextField)
+        scrollView.addSubview(tagCollectionView)
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.topAnchor),
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
-            tagCollectionView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: Metric.tagCollectionViewTop),
-            tagCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metric.tagCollectionViewLeading),
-            tagCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Metric.tagCollectionViewTrailing)
+            scrollView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            scrollView.contentLayoutGuide.topAnchor.constraint(equalTo: scrollView.frameLayoutGuide.topAnchor),
+            scrollView.contentLayoutGuide.leadingAnchor.constraint(equalTo: scrollView.frameLayoutGuide.leadingAnchor),
+            scrollView.contentLayoutGuide.trailingAnchor.constraint(equalTo: scrollView.frameLayoutGuide.trailingAnchor),
+            
+            urlTextField.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor, constant: Metric.urlTextFieldTop),
+            urlTextField.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor, constant: Metric.urlTextFieldLeading),
+            urlTextField.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor, constant: Metric.urlTextFieldTrailing),
+            
+            tagCollectionView.topAnchor.constraint(equalTo: urlTextField.bottomAnchor, constant: Metric.tagCollectionViewTop),
+            tagCollectionView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor, constant: Metric.tagCollectionViewLeading),
+            tagCollectionView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor, constant: Metric.tagCollectionViewTrailing),
         ])
     }
     
@@ -72,6 +104,18 @@ final class EnterBookmarkViewController: UIViewController, EnterBookmarkPresenta
         if tapGestureRecognizer.state == .ended { listener?.tagCollectionViewDidTap(existingSelectedTags: selectedTags) }
     }
 }
+
+// MARK: - UITextField
+
+extension EnterBookmarkViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        urlTextField.resignFirstResponder()
+        return true
+    }
+}
+
+// MARK: - UICollectionView
 
 extension EnterBookmarkViewController: UICollectionViewDataSource {
     
