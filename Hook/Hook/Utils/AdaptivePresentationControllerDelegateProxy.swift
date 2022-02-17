d2c785e4fce856bcef053e558e1ed6527c8fcc8e
meregrey <yeojinyoon.dev@gmail.com>
2022-02-17 22:56:01 +0900

    UIAdaptivePresentationControllerDelegate 콜백을 받아 델리게이트에 전달하는 AdaptivePresentationControllerDelegateProxy 클래스 정의

---
+++ Hook/Hook/Utils/AdaptivePresentationControllerDelegateProxy.swift
@@ -0,0 +1,21 @@
+//
+//  AdaptivePresentationControllerDelegateProxy.swift
+//  Hook
+//
+//  Created by Yeojin Yoon on 2022/02/17.
+//
+
+import UIKit
+
+protocol AdaptivePresentationControllerDelegate: AnyObject {
+    func presentationControllerDidDismiss()
+}
+
+final class AdaptivePresentationControllerDelegateProxy: NSObject, UIAdaptivePresentationControllerDelegate {
+    
+    weak var delegate: AdaptivePresentationControllerDelegate?
+    
+    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+        delegate?.presentationControllerDidDismiss()
+    }
+}
