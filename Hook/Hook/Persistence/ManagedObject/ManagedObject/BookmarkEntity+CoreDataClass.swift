696f3063fa54e72d5ed31b906ae8ddb6b795f26a
meregrey <yeojinyoon.dev@gmail.com>
2022-03-08 23:34:02 +0900

    BookmarkEntity 클래스에 변환 및 업데이트 관련 메서드 추가

--- Hook/Hook/Persistence/ManagedObject/ManagedObject/BookmarkEntity+CoreDataClass.swift
+++ Hook/Hook/Persistence/ManagedObject/ManagedObject/BookmarkEntity+CoreDataClass.swift
@@ -28,4 +28,24 @@ public class BookmarkEntity: NSManagedObject {
             addToTags(bookmarkTagEntity)
         }
     }
+    
+    func converted() -> Bookmark? {
+        guard let url = URL(string: self.urlString) else { return nil }
+        let bookmarkTagEntities = self.tags?.allObjects as? [BookmarkTagEntity]
+        let bookmarkTags = bookmarkTagEntities?.map { BookmarkTag(name: $0.name, index: Int($0.index)) }
+        return Bookmark(url: url, tags: bookmarkTags, note: self.note, title: self.title)
+    }
+    
+    func update(with bookmark: Bookmark) {
+        self.note = bookmark.note
+        
+        guard let tags = bookmark.tags else { return }
+        guard let context = managedObjectContext else { return }
+        
+        tags.forEach {
+            let bookmarkTagEntity = BookmarkTagEntity(context: context)
+            bookmarkTagEntity.configure(with: $0, bookmarkEntity: self)
+            addToTags(bookmarkTagEntity)
+        }
+    }
 }
