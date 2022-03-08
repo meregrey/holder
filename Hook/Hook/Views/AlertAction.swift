ebc4a8ea8aeb8465148372147db60950e80fc79c
meregrey <yeojinyoon.dev@gmail.com>
2022-03-08 23:34:17 +0900

    AlertAction 클래스의 액션 동작 시 핸들러 실행 후 델리게이트의 dismissAlert 메서드를 호출하도록 구현

--- Hook/Hook/Views/AlertAction.swift
+++ Hook/Hook/Views/AlertAction.swift
@@ -7,21 +7,39 @@
 
 import UIKit
 
+protocol AlertActionDelegate: AnyObject {
+    func dismissAlert()
+}
+
 final class AlertAction: UIButton {
     
-    init(title: String, handler: Selector) {
+    typealias Handler = () -> Void
+    
+    var handler: Handler
+    weak var delegate: AlertActionDelegate?
+    
+    init(title: String, handler: @escaping Handler) {
+        self.handler = handler
         super.init(frame: .zero)
-        configure(with: title, handler: handler)
+        configure(with: title)
     }
-
+    
     required init?(coder: NSCoder) {
+        self.handler = {}
         super.init(coder: coder)
+        configure(with: "")
     }
-
-    private func configure(with title: String, handler: Selector) {
+    
+    private func configure(with title: String) {
         titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
         setTitle(title, for: .normal)
         setTitleColor(Asset.Color.primaryColor, for: .normal)
-        addTarget(nil, action: handler, for: .touchUpInside)
+        addTarget(nil, action: #selector(executeHandler), for: .touchUpInside)
+    }
+    
+    @objc
+    private func executeHandler() {
+        handler()
+        delegate?.dismissAlert()
     }
 }
