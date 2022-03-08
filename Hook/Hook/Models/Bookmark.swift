a291462ccd80ca96a4432ad45ff6ef16c783a235
meregrey <yeojinyoon.dev@gmail.com>
2022-03-08 23:33:36 +0900

    Bookmark 구조체에 이니셜라이저 및 업데이트 관련 메서드 추가

--- Hook/Hook/Models/Bookmark.swift
+++ Hook/Hook/Models/Bookmark.swift
@@ -13,10 +13,18 @@ struct Bookmark {
     let note: String?
     let title: String?
     
+    func updated(tags: [Tag]?, note: String?) -> Self {
+        return Bookmark(url: url, tags: tags, note: note, title: title)
+    }
+    
+    func updated(title: String?) -> Self {
+        return Bookmark(url: url, tags: tags, note: note, title: title)
+    }
+}
+
+extension Bookmark {
     init(url: URL, tags: [Tag]?, note: String?, title: String?) {
-        self.url = url
-        self.tags = tags?.enumerated().map { BookmarkTag(name: $0.element.name, index: $0.offset) }
-        self.note = note
-        self.title = title
+        let tags = tags?.enumerated().map { BookmarkTag(name: $0.element.name, index: $0.offset) }
+        self.init(url: url, tags: tags, note: note, title: title)
     }
 }
