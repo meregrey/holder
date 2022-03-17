40a90a6bab47b9ba331e5f4f79d8ff9abaa35796
meregrey <yeojinyoon.dev@gmail.com>
2022-03-17 09:54:03 +0900

    앱 런치 시 인터페이스 스타일을 설정하도록 구현

--- Hook/Hook/App/AppDelegate.swift
+++ Hook/Hook/App/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RootListener {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        adoptInterfaceStyle()
         registerTransformer()
         verifyLoginState()
         launchRoot()
@@ -48,6 +49,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RootListener {
     
     func didRequestLogout() {
         credentialRepository.delete()
+    }
+    
+    private func adoptInterfaceStyle() {
+        guard let interfaceStyle = UserDefaults.value(forType: InterfaceStyle.self) else { return }
+        window?.adoptInterfaceStyle(interfaceStyle)
     }
     
     private func registerTransformer() {
