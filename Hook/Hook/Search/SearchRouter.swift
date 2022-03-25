9e4a2ebf2f980450da3267d5c09b93be7369b81b
meregrey <yeojinyoon.dev@gmail.com>
2022-03-25 12:57:56 +0900

    Search RIB 구현

--- Hook/Hook/Search/SearchRouter.swift
+++ Hook/Hook/Search/SearchRouter.swift
@@ -7,17 +7,175 @@
 
 import RIBs
 
-protocol SearchInteractable: Interactable {
+protocol SearchInteractable: Interactable, SearchBarListener, RecentSearchesListener, BookmarkListListener, BookmarkDetailListener, EnterBookmarkListener, SelectTagsListener, SearchTagsListener {
     var router: SearchRouting? { get set }
     var listener: SearchListener? { get set }
+    var presentationProxy: AdaptivePresentationControllerDelegateProxy { get }
 }
 
-protocol SearchViewControllable: ViewControllable {}
+protocol SearchViewControllable: ViewControllable {
+    func addChild(_ viewControllable: ViewControllable)
+    func removeChild(_ viewControllable: ViewControllable)
+    func push(_ view: ViewControllable)
+    func pop()
+    func presentOver(_ view: ViewControllable)
+    func dismissOver()
+}
 
 final class SearchRouter: ViewableRouter<SearchInteractable, SearchViewControllable>, SearchRouting {
     
-    override init(interactor: SearchInteractable, viewController: SearchViewControllable) {
+    private let searchBar: SearchBarBuildable
+    private let recentSearches: RecentSearchesBuildable
+    private let bookmarkList: BookmarkListBuildable
+    private let enterBookmark: EnterBookmarkBuildable
+    private let selectTags: SelectTagsBuildable
+    private let searchTags: SearchTagsBuildable
+    private let bookmarkDetail: BookmarkDetailBuildable
+    
+    private var searchBarRouter: SearchBarRouting?
+    private var recentSearchesRouter: RecentSearchesRouting?
+    private var bookmarkListRouter: BookmarkListRouting?
+    private var enterBookmarkRouter: EnterBookmarkRouting?
+    private var selectTagsRouter: SelectTagsRouting?
+    private var searchTagsRouter: SearchTagsRouting?
+    private var bookmarkDetailRouter: BookmarkDetailRouting?
+    
+    init(interactor: SearchInteractable,
+         viewController: SearchViewControllable,
+         searchBar: SearchBarBuildable,
+         recentSearches: RecentSearchesBuildable,
+         bookmarkList: BookmarkListBuildable,
+         enterBookmark: EnterBookmarkBuildable,
+         selectTags: SelectTagsBuildable,
+         searchTags: SearchTagsBuildable,
+         bookmarkDetail: BookmarkDetailBuildable) {
+        self.searchBar = searchBar
+        self.recentSearches = recentSearches
+        self.bookmarkList = bookmarkList
+        self.enterBookmark = enterBookmark
+        self.selectTags = selectTags
+        self.searchTags = searchTags
+        self.bookmarkDetail = bookmarkDetail
         super.init(interactor: interactor, viewController: viewController)
         interactor.router = self
+    }
+    
+    // MARK: - SearchBar
+    
+    func attachSearchBar() {
+        guard searchBarRouter == nil else { return }
+        let router = searchBar.build(withListener: interactor)
+        searchBarRouter = router
+        attachChild(router)
+        viewController.addChild(router.viewControllable)
+    }
+    
+    // MARK: - RecentSearches
+    
+    func attachRecentSearches() {
+        guard recentSearchesRouter == nil else { return }
+        let router = recentSearches.build(withListener: interactor)
+        recentSearchesRouter = router
+        attachChild(router)
+        viewController.addChild(router.viewControllable)
+    }
+    
+    func attachRecentSearchesView() {
+        guard let router = recentSearchesRouter else { return }
+        viewController.addChild(router.viewControllable)
+    }
+    
+    func detachRecentSearchesView() {
+        guard let router = recentSearchesRouter else { return }
+        viewController.removeChild(router.viewControllable)
+    }
+    
+    // MARK: - BookmarkList
+    
+    func attachBookmarkList() {
+        guard bookmarkListRouter == nil else { return }
+        let router = bookmarkList.build(withListener: interactor)
+        bookmarkListRouter = router
+        attachChild(router)
+        viewController.addChild(router.viewControllable)
+    }
+    
+    func detachBookmarkList() {
+        guard let router = bookmarkListRouter else { return }
+        viewController.removeChild(router.viewControllable)
+        detachChild(router)
+        bookmarkListRouter = nil
+    }
+    
+    // MARK: - BookmarkDetail
+    
+    func attachBookmarkDetail(bookmarkEntity: BookmarkEntity) {
+        guard bookmarkDetailRouter == nil else { return }
+        let router = bookmarkDetail.build(withListener: interactor, bookmarkEntity: bookmarkEntity)
+        bookmarkDetailRouter = router
+        attachChild(router)
+        viewController.push(router.viewControllable)
+    }
+    
+    func detachBookmarkDetail(includingView isViewIncluded: Bool) {
+        guard let router = bookmarkDetailRouter else { return }
+        if isViewIncluded { viewController.pop() }
+        detachChild(router)
+        bookmarkDetailRouter = nil
+    }
+    
+    // MARK: - EnterBookmark
+    
+    func attachEnterBookmark(mode: EnterBookmarkMode) {
+        guard enterBookmarkRouter == nil else { return }
+        let router = enterBookmark.build(withListener: interactor, mode: mode)
+        enterBookmarkRouter = router
+        attachChild(router)
+        router.viewControllable.uiviewController.presentationController?.delegate = interactor.presentationProxy
+        viewController.present(router.viewControllable, modalPresentationStyle: .pageSheet, animated: true)
+    }
+    
+    func detachEnterBookmark(includingView isViewIncluded: Bool) {
+        guard let router = enterBookmarkRouter else { return }
+        if isViewIncluded { viewController.dismiss(animated: true) }
+        detachChild(router)
+        enterBookmarkRouter = nil
+    }
+    
+    // MARK: - SelectTags
+    
+    func attachSelectTags(existingSelectedTags: [Tag]) {
+        guard selectTagsRouter == nil else { return }
+        let router = selectTags.build(withListener: interactor, existingSelectedTags: existingSelectedTags)
+        selectTagsRouter = router
+        attachChild(router)
+        viewController.presentOver(router.viewControllable)
+    }
+    
+    func detachSelectTags() {
+        guard let router = selectTagsRouter else { return }
+        viewController.dismissOver()
+        detachChild(router)
+        selectTagsRouter = nil
+    }
+    
+    // MARK: - SearchTags
+    
+    func attachSearchTags() {
+        guard searchTagsRouter == nil else { return }
+        guard let selectTagsRouter = selectTagsRouter else { return }
+        let router = searchTags.build(withListener: interactor)
+        searchTagsRouter = router
+        attachChild(router)
+        selectTagsRouter.viewControllable.present(router.viewControllable,
+                                                  modalPresentationStyle: .currentContext,
+                                                  animated: true)
+    }
+    
+    func detachSearchTags() {
+        guard let router = searchTagsRouter else { return }
+        router.viewControllable.dismiss(animated: true)
+        detachChild(router)
+        searchTagsRouter = nil
     }
 }
--- Hook/Hook/Search/SearchViewController.swift
+++ Hook/Hook/Search/SearchViewController.swift
@@ -12,6 +12,32 @@ protocol SearchPresentableListener: AnyObject {}
 
 final class SearchViewController: UIViewController, SearchPresentable, SearchViewControllable {
     
+    @AutoLayout private var contentContainerView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        return stackView
+    }()
+    
+    @AutoLayout private var searchBarContainerView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        return stackView
+    }()
+    
+    @AutoLayout private var dummyView = UIView()
+    
+    private lazy var contentContainerViewHeightConstraint = NSLayoutConstraint(item: contentContainerView,
+                                                                               attribute: .height,
+                                                                               relatedBy: .equal,
+                                                                               toItem: nil,
+                                                                               attribute: .notAnAttribute,
+                                                                               multiplier: 1,
+                                                                               constant: 0)
+    
     private enum Image {
         static let tabBarItem = UIImage(named: "search")
     }
@@ -20,19 +46,121 @@ final class SearchViewController: UIViewController, SearchPresentable, SearchVie
     
     init() {
         super.init(nibName: nil, bundle: nil)
+        registerToReceiveNotification()
         configureViews()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        registerToReceiveNotification()
         configureViews()
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        contentContainerViewHeightConstraint.constant = view.frame.height - view.safeAreaInsets.bottom
+    }
+    
+    func addChild(_ viewControllable: ViewControllable) {
+        let childViewController = viewControllable.uiviewController
+        addChild(childViewController)
+        addChildView(of: childViewController)
+        childViewController.didMove(toParent: self)
+    }
+    
+    func removeChild(_ viewControllable: ViewControllable) {
+        let childViewController = viewControllable.uiviewController
+        if childViewController is SearchBarViewController { return }
+        childViewController.willMove(toParent: nil)
+        childViewController.view.removeFromSuperview()
+        childViewController.removeFromParent()
+    }
+    
+    func push(_ viewControllable: ViewControllable) {
+        navigationController?.pushViewController(viewControllable.uiviewController, animated: true)
+    }
+    
+    func pop() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func presentOver(_ viewControllable: ViewControllable) {
+        let viewController = viewControllable.uiviewController
+        viewController.modalPresentationStyle = .currentContext
+        presentedViewController?.present(viewController, animated: true)
+    }
+    
+    func dismissOver() {
+        presentedViewController?.dismiss(animated: true)
+    }
+    
+    private func registerToReceiveNotification() {
+        NotificationCenter.addObserver(self,
+                                       selector: #selector(keyboardWillShow(_:)),
+                                       name: UIResponder.keyboardWillShowNotification)
+        
+        NotificationCenter.addObserver(self,
+                                       selector: #selector(keyboardWillHide),
+                                       name: UIResponder.keyboardWillHideNotification)
+    }
+    
+    @objc
+    private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        let constant = view.frame.height - keyboardFrame.height
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.2, delay: 0, options: .curveLinear) {
+            self.contentContainerViewHeightConstraint.constant = constant
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc
+    private func keyboardWillHide() {
+        let constant = view.frame.height - view.safeAreaInsets.bottom
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.2, delay: 0, options: .curveLinear) {
+            self.contentContainerViewHeightConstraint.constant = constant
+            self.view.layoutIfNeeded()
+        }
+    }
+    
     private func configureViews() {
-        title = LocalizedString.ViewTitle.search
         tabBarItem = UITabBarItem(title: nil,
                                   image: Image.tabBarItem,
                                   selectedImage: Image.tabBarItem)
+        
         view.backgroundColor = Asset.Color.baseBackgroundColor
+        
+        view.addSubview(contentContainerView)
+        view.addSubview(searchBarContainerView)
+        contentContainerView.addArrangedSubview(dummyView)
+        
+        contentContainerViewHeightConstraint.isActive = true
+        
+        NSLayoutConstraint.activate([
+            contentContainerView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            searchBarContainerView.topAnchor.constraint(equalTo: view.topAnchor),
+            searchBarContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBarContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            dummyView.heightAnchor.constraint(equalToConstant: 0)
+        ])
+    }
+    
+    private func addChildView(of childViewController: UIViewController) {
+        let containerView = childViewController is SearchBarViewController ? searchBarContainerView : contentContainerView
+        containerView.addArrangedSubview(childViewController.view)
     }
 }
