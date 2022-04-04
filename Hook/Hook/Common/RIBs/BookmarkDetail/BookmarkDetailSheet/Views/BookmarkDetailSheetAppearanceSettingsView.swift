ccec9a8ff41e177054010c5bd52bdcb1454e3366
meregrey <yeojinyoon.dev@gmail.com>
2022-04-04 15:05:39 +0900

    BookmarkDetailSheetAppearanceSettingsOptionView를 ChoiceView로 변경

--- Hook/Hook/Common/RIBs/BookmarkDetail/BookmarkDetailSheet/Views/BookmarkDetailSheetAppearanceSettingsView.swift
+++ Hook/Hook/Common/RIBs/BookmarkDetail/BookmarkDetailSheet/Views/BookmarkDetailSheetAppearanceSettingsView.swift
@@ -19,9 +19,9 @@ final class BookmarkDetailSheetAppearanceSettingsView: RoundedCornerView {
         return stackView
     }()
     
-    @AutoLayout private var systemSettingView = BookmarkDetailSheetAppearanceSettingsOptionView(title: LocalizedString.ActionTitle.systemSetting)
-    @AutoLayout private var lightView = BookmarkDetailSheetAppearanceSettingsOptionView(title: LocalizedString.ActionTitle.light)
-    @AutoLayout private var darkView = BookmarkDetailSheetAppearanceSettingsOptionView(title: LocalizedString.ActionTitle.dark)
+    @AutoLayout private var systemSettingChoiceView = ChoiceView(title: LocalizedString.ActionTitle.systemSetting)
+    @AutoLayout private var lightChoiceView = ChoiceView(title: LocalizedString.ActionTitle.light)
+    @AutoLayout private var darkChoiceView = ChoiceView(title: LocalizedString.ActionTitle.dark)
     
     private enum Metric {
         static let stackViewTop = CGFloat(24)
@@ -41,18 +41,18 @@ final class BookmarkDetailSheetAppearanceSettingsView: RoundedCornerView {
     }
     
     private func configure() {
-        checkInterfaceStyle()
+        select()
         
-        systemSettingView.addTarget(self, action: #selector(systemSettingViewDidTap))
-        lightView.addTarget(self, action: #selector(lightViewDidTap))
-        darkView.addTarget(self, action: #selector(darkViewDidTap))
+        systemSettingChoiceView.addTarget(self, action: #selector(systemSettingChoiceViewDidTap))
+        lightChoiceView.addTarget(self, action: #selector(lightChoiceViewDidTap))
+        darkChoiceView.addTarget(self, action: #selector(darkChoiceViewDidTap))
         
         backgroundColor = Asset.Color.sheetBaseBackgroundColor
         
         addSubview(stackView)
-        stackView.addArrangedSubview(systemSettingView)
-        stackView.addArrangedSubview(lightView)
-        stackView.addArrangedSubview(darkView)
+        stackView.addArrangedSubview(systemSettingChoiceView)
+        stackView.addArrangedSubview(lightChoiceView)
+        stackView.addArrangedSubview(darkChoiceView)
         
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: topAnchor, constant: Metric.stackViewTop),
@@ -62,90 +62,31 @@ final class BookmarkDetailSheetAppearanceSettingsView: RoundedCornerView {
         ])
     }
     
-    private func checkInterfaceStyle() {
+    private func select() {
         let interfaceStyle = UserDefaults.value(forType: InterfaceStyle.self) ?? .systemSetting
-        systemSettingView.check(interfaceStyle == .systemSetting)
-        lightView.check(interfaceStyle == .light)
-        darkView.check(interfaceStyle == .dark)
+        systemSettingChoiceView.select(interfaceStyle == .systemSetting)
+        lightChoiceView.select(interfaceStyle == .light)
+        darkChoiceView.select(interfaceStyle == .dark)
+    }
+    
+    @objc
+    private func systemSettingChoiceViewDidTap() {
+        handleInterfaceStyle(.systemSetting)
+    }
+    
+    @objc
+    private func lightChoiceViewDidTap() {
+        handleInterfaceStyle(.light)
+    }
+    
+    @objc
+    private func darkChoiceViewDidTap() {
+        handleInterfaceStyle(.dark)
     }
     
     private func handleInterfaceStyle(_ interfaceStyle: InterfaceStyle) {
         window?.adoptInterfaceStyle(interfaceStyle)
         UserDefaults.set(interfaceStyle)
-        checkInterfaceStyle()
-    }
-    
-    @objc
-    private func systemSettingViewDidTap() {
-        handleInterfaceStyle(.systemSetting)
-    }
-    
-    @objc
-    private func lightViewDidTap() {
-        handleInterfaceStyle(.light)
-    }
-    
-    @objc
-    private func darkViewDidTap() {
-        handleInterfaceStyle(.dark)
-    }
-}
-
-private final class BookmarkDetailSheetAppearanceSettingsOptionView: UIView {
-    
-    @AutoLayout private var titleButton: UIButton = {
-        let button = UIButton()
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
-        button.setTitleColor(Asset.Color.primaryColor, for: .normal)
-        return button
-    }()
-    
-    @AutoLayout private var checkmarkImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "checkmark", withConfiguration: UIImage.SymbolConfiguration(weight: .semibold))
-        imageView.tintColor = Asset.Color.primaryColor
-        imageView.contentMode = .scaleAspectFit
-        imageView.isHidden = true
-        return imageView
-    }()
-    
-    private enum Metric {
-        static let checkmarkImageViewWidthHeight = CGFloat(22)
-    }
-    
-    init(title: String) {
-        super.init(frame: .zero)
-        configure(with: title)
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        configure(with: "")
-    }
-    
-    func addTarget(_ target: Any?, action: Selector) {
-        titleButton.addTarget(target, action: action, for: .touchUpInside)
-    }
-    
-    func check(_ flag: Bool) {
-        checkmarkImageView.isHidden = !flag
-    }
-    
-    private func configure(with title: String) {
-        titleButton.setTitle(title, for: .normal)
-        
-        addSubview(titleButton)
-        addSubview(checkmarkImageView)
-        
-        NSLayoutConstraint.activate([
-            titleButton.topAnchor.constraint(equalTo: topAnchor),
-            titleButton.leadingAnchor.constraint(equalTo: leadingAnchor),
-            titleButton.bottomAnchor.constraint(equalTo: bottomAnchor),
-            
-            checkmarkImageView.widthAnchor.constraint(equalToConstant: Metric.checkmarkImageViewWidthHeight),
-            checkmarkImageView.heightAnchor.constraint(equalToConstant: Metric.checkmarkImageViewWidthHeight),
-            checkmarkImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            checkmarkImageView.trailingAnchor.constraint(equalTo: trailingAnchor)
-        ])
+        select()
     }
 }
