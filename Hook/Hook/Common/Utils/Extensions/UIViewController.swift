2fc4292447d0e0b79d5bdbbcd23991ba85973035
meregrey <yeojinyoon.dev@gmail.com>
2022-04-11 14:04:40 +0900

    UIViewController 익스텐션을 통해 preferredStatusBarStyle 지정

---
+++ Hook/Hook/Common/Utils/Extensions/UIViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController.swift
+//  Hook
+//
+//  Created by Yeojin Yoon on 2022/04/11.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    var preferredStatusBarStyle: UIStatusBarStyle { .default }
+}
