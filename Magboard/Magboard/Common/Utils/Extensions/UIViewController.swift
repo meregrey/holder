34e80f0cef8b7b2d16fec518fee524aac7d9a43a
meregrey <yeojinyoon.dev@gmail.com>
2022-04-13 13:15:38 +0900

    UIViewController 익스텐션을 통해 preferredStatusBarStyle 지정

---
+++ Magboard/Magboard/Common/Utils/Extensions/UIViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController.swift
+//  Magboard
+//
+//  Created by Yeojin Yoon on 2022/04/13.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    var preferredStatusBarStyle: UIStatusBarStyle { .default }
+}
