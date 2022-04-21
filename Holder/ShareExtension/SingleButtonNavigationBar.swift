8f9c7ca572b85574458d1063bc3aaa22cd53c314
meregrey <yeojinyoon.dev@gmail.com>
2022-04-21 19:09:35 +0900

    하나의 취소 버튼을 표시하는 SingleButtonNavigationBar 클래스 정의

---
+++ Holder/ShareExtension/SingleButtonNavigationBar.swift
@@ -0,0 +1,52 @@
+//
+//  SingleButtonNavigationBar.swift
+//  ShareExtension
+//
+//  Created by Yeojin Yoon on 2022/04/20.
+//
+
+import UIKit
+
+protocol SingleButtonNavigationBarListener: AnyObject {
+    func cancelButtonDidTap()
+}
+
+final class SingleButtonNavigationBar: UINavigationBar {
+    
+    private let item = UINavigationItem()
+    
+    private let cancelButton = UIBarButtonItem(title: LocalizedString.ActionTitle.cancel, style: .plain, target: nil, action: #selector(cancelButtonDidTap))
+    
+    private let appearance: UINavigationBarAppearance = {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = Asset.Color.sheetBaseBackgroundColor
+        return appearance
+    }()
+    
+    weak var listener: SingleButtonNavigationBarListener?
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        configure(with: title)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure(with: "")
+    }
+    
+    private func configure(with title: String) {
+        item.title = title
+        item.rightBarButtonItem = cancelButton
+        items = [item]
+        standardAppearance = appearance
+        scrollEdgeAppearance = appearance
+        tintColor = Asset.Color.primaryColor
+    }
+    
+    @objc
+    private func cancelButtonDidTap() {
+        listener?.cancelButtonDidTap()
+    }
+}
