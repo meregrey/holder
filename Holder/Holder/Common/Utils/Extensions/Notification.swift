61d4af5c2604d8f764abef016e347474b0ea85d9
meregrey <yeojinyoon.dev@gmail.com>
2022-05-11 19:44:40 +0900

    익스텐션을 통해 Notification.UserInfoKey 열거형 정의

---
+++ Holder/Holder/Common/Utils/Extensions/Notification.swift
@@ -0,0 +1,14 @@
+//
+//  Notification.swift
+//  Holder
+//
+//  Created by Yeojin Yoon on 2022/05/11.
+//
+
+import Foundation
+
+extension Notification {
+    enum UserInfoKey {
+        static let searchTerm = "searchTerm"
+    }
+}
